0986c1b54cffbad8aa103fe1592b6b8dd36c59eb
HyaraTavares <hyara.tavares@mail.uft.edu.br>
2025-07-01 13:04:12 -0300

    categories

--- src/main/java/com/projetopoo/curso/config/TestConfig.java
+++ src/main/java/com/projetopoo/curso/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.projetopoo.curso.entities.Category;
 import com.projetopoo.curso.entities.Order;
 import com.projetopoo.curso.entities.User;
 import com.projetopoo.curso.entities.enums.OrderStatus;
+import com.projetopoo.curso.repositories.CategoryRepository;
 import com.projetopoo.curso.repositories.OrderRepository;
 import com.projetopoo.curso.repositories.UserRepository;
 
@@ -25,8 +27,16 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private  CategoryRepository  categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+
 	User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 	User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 	
