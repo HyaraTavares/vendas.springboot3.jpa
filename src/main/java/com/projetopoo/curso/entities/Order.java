83ec28c1b47b8b8d2c446a9b27f17e0a82083369
HyaraTavares <hyara.tavares@mail.uft.edu.br>
2025-07-01 12:15:51 -0300

    enum

--- src/main/java/com/projetopoo/curso/entities/Order.java
+++ src/main/java/com/projetopoo/curso/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projetopoo.curso.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +27,7 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private OrderStatus orderStatus;
 //um cliente pode ter varios pedidos mas um order só pode ser de um cliente
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -33,10 +35,11 @@ public class Order implements Serializable{
 	
 	public Order() {}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		this.orderStatus = orderStatus;
 		this.client = client;
 	}
 
@@ -54,6 +57,15 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+	
+	public OrderStatus getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus;
 	}
 
 	public User getClient() {
---
+++ src/main/java/com/projetopoo/curso/entities/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.projetopoo.curso.entities.enums;
+
+public enum OrderStatus {
+	WAITING_PAYMENT,PAID,SHIPPED,DELIVERED,CANCELED;
+}
